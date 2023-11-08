c987614ed4e14f2dd69a63a4e5a0c8520b8baa92
Akshay Krishna <akshay.krishna@qburst.com>
2023-11-08 13:44:29 +0530

    Implemented React.Lazy

--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -1,11 +1,45 @@
 import { createBrowserRouter } from "react-router-dom";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import ViewDetails from "../pages/ViewDetails/ViewDetails";
-import AddEdit from "../pages/AddEdit/AddEdit";
+// import Dashboard from "../pages/Dashboard/Dashboard";
+// import ViewDetails from "../pages/ViewDetails/ViewDetails";
+// import AddEdit from "../pages/AddEdit/AddEdit";
+import React from "react";
+const LazyDashboard = React.lazy(() => import("../pages/Dashboard/Dashboard"));
+const LazyViewDetails = React.lazy(
+  () => import("../pages/ViewDetails/ViewDetails")
+);
+const LazyAddEdit = React.lazy(() => import("../pages/AddEdit/AddEdit"));
 
 const Routing = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
-  { path: "/employee-detail/", element: <ViewDetails /> },
-  { path: "/add_edit_page", element: <AddEdit /> },
+  {
+    path: "/",
+    element: (
+      <>
+        <React.Suspense>
+          <LazyDashboard />
+        </React.Suspense>{" "}
+      </>
+    ),
+  },
+
+  {
+    path: "/employee-detail/",
+    element: (
+      <>
+        <React.Suspense>
+          <LazyViewDetails />
+        </React.Suspense>{" "}
+      </>
+    ),
+  },
+  {
+    path: "/add_edit_page",
+    element: (
+      <>
+        <React.Suspense>
+          <LazyAddEdit />
+        </React.Suspense>{" "}
+      </>
+    ),
+  },
 ]);
 export default Routing;
